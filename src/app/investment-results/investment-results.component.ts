9692a840ab697190f0f2f0b57b722cdc58aafc98
TryastsinEugene <eugenetryas@gmail.com>
2026-02-09 15:45:01 +0200

    Added service

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input } from '@angular/core';
-import { InvestmentInput } from '../investment-input.model';
+import { Component, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-   @Input() results?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
+  service = inject(InvestmentService);
+
+  get results(){
+   return this.service.investmentData;
+  } 
+ 
 }
